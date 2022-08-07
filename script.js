da274aa57755122b2c77c04b84e9c096f18d0225
kunal-arya <85951446+kunal-arya@users.noreply.github.com>
2022-08-07 10:55:45 +0530

    Refactor: refactor the code
    Refactor the code by using constructor function

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 
-
-const hero = {
-    elementId : "hero",
-    name: "Wizard",
-    avatar: "./images/wizard.png",
-    health: 60,
-    diceCounts : 3
-}
-
-const monster = {
-    elementId : "monster",
-    name: "Orc",
-    avatar: "./images/orc.png",
-    health: 10,
-    diceCounts: 1
+const charData = {
+    hero: {
+        name: "Wizard",
+        avatar: "./images/wizard.png",
+        health: 60,
+        diceCounts : 3
+    },
+    monster: {
+        name: "Orc",
+        avatar: "./images/orc.png",
+        health: 10,
+        diceCounts: 1
+    }
 }
 
 // to generate random dice possibilties
-function getDiceRoll(diceCount) {
+function getDiceRollArray(diceCount) {
     return new Array(diceCount).fill(0).map( () => {
         return Math.floor(Math.random() * 6) + 1;
     })
 }
 
-function getDiceHtml(diceCount) {
-    return getDiceRoll(diceCount).map( dice => {
-        return `<div class="dice"> ${dice} </div>`
-    }).join('');
+function Character(data){
+    Object.assign(this,data);
+    this.getDiceHtml = () => getDiceRollArray(this.diceCounts).map( dice => `<div class="dice"> ${dice} </div>`).join('');
+
+    this.getCharacterhtml = function(){
+        const {elementId, name , avatar , health , diceCounts } = this;
+        
+        return `
+        <div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src= ${avatar}  alt="hero Image" />
+            <div class="health"> health: <span> ${health} </span></div>
+            <div class="dice-container">
+            ${this.getDiceHtml()}
+            </div>
+        </div>
+        `
+    }
 }
 
+function render() {
+    document.getElementById("hero").innerHTML = wizard.getCharacterhtml();
+    document.getElementById("monster").innerHTML = orc.getCharacterhtml();
+}
 
-// render the character on the page
-function renderCharacter(data) {
-    const {elementId, name , avatar , health , diceCounts } = data;
-    document.getElementById(elementId).innerHTML = `
-    <div class="character-card">
-        <h4 class="name"> ${name} </h4>
-        <img class="avatar" src= ${avatar}  alt="hero Image" />
-        <div class="health"> health: <span> ${health} </span></div>
-        <div class="dice-container">
-        ${getDiceHtml(diceCounts)}
-        </div>
-    </div>
-    `
-} 
-
-renderCharacter(hero);
-renderCharacter(monster)
-
-
+const wizard = new Character(charData.hero);
+const orc = new Character(charData.monster);
+render();
